b54b01295338da9a29af653b4415a338cf556664
DavidStinson <49245298+DavidStinson@users.noreply.github.com>
2021-04-30 13:27:30 -0500

    finish gatsby node

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,40 @@
-exports.onCreateNode = ({ node, getNode }) => {
+const { createFilePath } = require(`gatsby-source-filesystem`)
+const path = require(`path`)
+
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions
   if (node.internal.type === `MarkdownRemark`) {
-    const fileNode = getNode(node.parent)
-    console.log(`\n`, fileNode.relativePath)
+    const slug = createFilePath({node, getNode, basePath: `posts`})
+    createNodeField({
+      node,
+      name: `slug`,
+      value: `posts${slug}`
+    })
   }
+}
+
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions 
+	const result = await graphql(`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `)
+  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    createPage({
+      path: node.fields.slug,
+      component: path.resolve(`./src/templates/post.jsx`),
+      context: {
+        slug: node.fields.slug,
+      }
+    })
+  })
 }
---
+++ src/templates/post.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import Layout from "../components/layout"
+
+export default function Post() {
+  return (
+    <Layout>
+      <h1>There is a post here!</h1>
+    </Layout>
+  )
+}
