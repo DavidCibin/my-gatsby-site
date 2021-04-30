4050ab6ff2195212acd26569149de8c2645b122b
DavidStinson <49245298+DavidStinson@users.noreply.github.com>
2021-04-30 09:22:05 -0500

    up to finding fonts

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,8 +4,9 @@ import { Link } from "gatsby"
 
 export default function Home() {
   return (
-    <div style={{ color: "purple", fontFamily: "sans-serif" }}>
+    <div style={{ color: "purple", fontFamily: "Droid Serif" }}>
       <Link to="/contact">Contact Me</Link> 
+      <Link to="/my-random-pic">A random pic</Link> 
       <Header headerText="Index: A place to be."/>
       Hello, friend.
     </div>
